bf5cd95967665a6591e679343695a48e0afb6e7e
Saranya2312 <saranya.roop@gmail.com>
2016-01-27 22:48:18 -0500

    Data collection using Bing APIs

---
+++ bing_data_collection.py
@@ -0,0 +1,38 @@
+#!/path/to/interpreter
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import urllib
+import urllib2
+import json
+
+def main():
+    query = "#landslides posts site:facebook.com"
+    # &tbs=qdr:w results of previous week
+    # disaster NOT musicband
+    # use - to exclude a site eg : wiki-wikipedia.org
+    print bing_search(query, 'Web')
+    #print bing_search(query, 'Image')
+
+def bing_search(query, search_type):
+    #search_type: Web, Image, News, Video
+    key= 'sRgVPBZi+zZzBkEw/1eCEsvt5p7Z9HnhDv+fGoLkB28'
+    query = urllib.quote(query)
+    # create credential for authentication
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
+    credentials = (':%s' % key).encode('base64')[:-1]
+    auth = 'Basic %s' % credentials
+    url = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=5&$format=json'
+    request = urllib2.Request(url)
+    request.add_header('Authorization', auth)
+    request.add_header('User-Agent', user_agent)
+    request_opener = urllib2.build_opener()
+    response = request_opener.open(request)
+    response_data = response.read()
+    json_result = json.loads(response_data)
+    result_list = json_result['d']['results']
+    print result_list
+    return result_list
+
+if __name__ == "__main__":
+    main()
+
